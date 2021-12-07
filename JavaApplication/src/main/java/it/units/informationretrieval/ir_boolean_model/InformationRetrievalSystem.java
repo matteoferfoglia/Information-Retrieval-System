f9310ccef2b66ea5b6b14618adc14a3dc9e22285
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-12-07 16:33:29 +0100

    Remove useless output message from constructor of InformationRetrievalSystem.

--- JavaApplication/src/main/java/it/units/informationretrieval/ir_boolean_model/InformationRetrievalSystem.java
+++ JavaApplication/src/main/java/it/units/informationretrieval/ir_boolean_model/InformationRetrievalSystem.java
@@ -32,7 +32,7 @@ public class InformationRetrievalSystem implements Serializable {
      * @param corpus The {@link Corpus} to use in this {@link InformationRetrievalSystem}.
      */
     public InformationRetrievalSystem(@NotNull Corpus corpus) {
-        this.corpus = Objects.requireNonNull(corpus, "The corpus cannot be null");
+        this.corpus = Objects.requireNonNull(corpus);
         this.invertedIndex = new InvertedIndex(corpus);
     }
 
