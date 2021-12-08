e74437100a8730a22a17ab5c038e66bc1ee687e3
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-12-08 12:47:30 +0100

    Add TODO

--- JavaApplication/src/main/java/it/units/informationretrieval/ir_boolean_model/entities/Document.java
+++ JavaApplication/src/main/java/it/units/informationretrieval/ir_boolean_model/entities/Document.java
@@ -16,7 +16,7 @@ import java.util.logging.Logger;
  *
  * @author Matteo Ferfoglia
  */
-public abstract class Document implements Serializable, Comparable<Document> {
+public abstract class Document implements Serializable, Comparable<Document> {  // TODO: simplify creation of documents
 
     /**
      * The title of the document.
