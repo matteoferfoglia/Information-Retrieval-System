71e892511abfc9c9592dd94376c292998809c3df
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-12-08 19:40:46 +0100

    Add TODO

--- JavaApplication/src/test/java/Benchmarking.java
+++ JavaApplication/src/test/java/Benchmarking.java
@@ -2,6 +2,7 @@ import benchmark.BenchmarkRunner;
 
 public class Benchmarking {
     public static void main(String[] args) {    // TODO: create a Maven plugin to run this automatically
+        // TODO: this method may be invoked from production code (e.g., a flag as command line arg to main)
         BenchmarkRunner benchmarkRunner = new BenchmarkRunner();
         benchmarkRunner.benchmarkAllAnnotatedMethodsAndGetListOfResults();
         System.out.println(benchmarkRunner);
