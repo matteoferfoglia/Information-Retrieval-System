0bea5f9044395fd921b2b23ca607df02b90ed9e0
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-12-14 19:17:36 +0100

    Now Benchmarking write results to file

--- JavaApplication/src/test/java/Benchmarking.java
+++ JavaApplication/src/test/java/Benchmarking.java
@@ -1,10 +1,23 @@
 import benchmark.BenchmarkRunner;
+import it.units.informationretrieval.ir_boolean_model.utils.Utility;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Instant;
 
 public class Benchmarking {
-    public static void main(String[] args) {    // TODO: create a Maven plugin to run this automatically
+
+    private static final String FOLDER_NAME_TO_SAVE_RESULTS = "benchmarks";
+
+    public static void main(String[] args) throws IOException {    // TODO: create a Maven plugin to run this automatically
         // TODO: this method may be invoked from production code (e.g., a flag as command line arg to main)
         BenchmarkRunner benchmarkRunner = new BenchmarkRunner();
         benchmarkRunner.benchmarkAllAnnotatedMethodsAndGetListOfResults();
         System.out.println(benchmarkRunner);
+        Utility.writeToFile(
+                benchmarkRunner.toString(),
+                new File(FOLDER_NAME_TO_SAVE_RESULTS + File.separator + Instant.now()),
+                false);
     }
+
 }
