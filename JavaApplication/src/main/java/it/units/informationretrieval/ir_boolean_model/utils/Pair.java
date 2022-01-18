55d85567593cbcd16415389e86cbd542244f910d
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2022-01-18 21:31:38 +0100

    Add method Pair.toString

--- JavaApplication/src/main/java/it/units/informationretrieval/ir_boolean_model/utils/Pair.java
+++ JavaApplication/src/main/java/it/units/informationretrieval/ir_boolean_model/utils/Pair.java
@@ -3,12 +3,17 @@ package it.units.informationretrieval.ir_boolean_model.utils;
 import java.util.AbstractMap;
 import java.util.Map;
 
-public class Pair<K, V> extends AbstractMap.SimpleEntry<K,V> {
+public class Pair<K, V> extends AbstractMap.SimpleEntry<K, V> {
     public Pair(K key, V value) {
         super(key, value);
     }
 
     public Pair(Map.Entry<? extends K, ? extends V> entry) {
         super(entry);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + getKey() + "," + getValue() + ")";
     }
 }
