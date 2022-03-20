1bf2dea7c7eb1a31e88caaa36f51afe55b6139a8
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2022-03-20 01:26:09 +0100

    Update serialization to avoid heap error (not working)

--- JavaApplication/src/main/java/it/units/informationretrieval/ir_boolean_model/InformationRetrievalSystem.java
+++ JavaApplication/src/main/java/it/units/informationretrieval/ir_boolean_model/InformationRetrievalSystem.java
@@ -5,7 +5,10 @@ import it.units.informationretrieval.ir_boolean_model.queries.BooleanExpression;
 import org.jetbrains.annotations.NotNull;
 import skiplist.SkipList;
 
-import java.io.Serializable;
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
@@ -15,19 +18,19 @@ import java.util.Objects;
  *
  * @author Matteo Ferfoglia
  */
-public class InformationRetrievalSystem implements Serializable {
+public class InformationRetrievalSystem implements Externalizable {
 
     /**
      * The {@link Corpus}.
      */
     @NotNull
-    private final Corpus corpus;
+    private Corpus corpus;
 
     /**
      * The {@link InvertedIndex}.
      */
     @NotNull
-    private final InvertedIndex invertedIndex;
+    private InvertedIndex invertedIndex;
 
     /**
      * Constructor.
@@ -47,6 +50,9 @@ public class InformationRetrievalSystem implements Serializable {
     public InformationRetrievalSystem(@NotNull InvertedIndex invertedIndex) {
         this.invertedIndex = invertedIndex;
         this.corpus = invertedIndex.getCorpus();
+    }
+
+    public InformationRetrievalSystem() {
     }
 
     /**
@@ -154,4 +160,14 @@ public class InformationRetrievalSystem implements Serializable {
         return invertedIndex.cf(str);
     }
 
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeObject(invertedIndex);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        invertedIndex = (InvertedIndex) in.readObject();
+        corpus = invertedIndex.getCorpus();
+    }
 }
